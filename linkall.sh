2acb861b3796b44d63da3af392e578b4b21d9850
Stephen Holtz <stephenlholtz@gmail.com>
2014-05-03 12:26:24 -0400

    linkall changed

--- linkall.sh
+++ linkall.sh
@@ -1,8 +1,11 @@
-# Link all of my configuration files from repo to ~
-echo 'sym linking github config files to home directory'
-rm ~/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc}
+# Link configuration files to home directory
+
 sp=$(dirname $0)
-ln -sv $sp/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc} ~/
+cp=$(pwd)
+echo 'sym-linking config files' $sp 'to' $HOME
+rm $HOME/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc}
+ln -sv $sp/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc} $HOME
 
 # Link matlab startup file for correct startup config
-ln -sv $sp/startup.m ~/Documents/MATLAB/
+rm $HOME/Documents/MATLAB/startup.m
+ln -sv $sp/startup.m $HOME/Documents/MATLAB/startup.m
