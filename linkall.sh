7ef9bf15ffa58a8ce0690a0c3cd022899f1dbfe1
Stephen Holtz <stephenlholtz@gmail.com>
2014-05-03 10:27:10 -0400

    Temp

--- linkall.sh
+++ linkall.sh
@@ -1,6 +1,6 @@
 # Link all of my configuration files from repo to ~
 echo 'sym linking github config files to home directory'
-#rm ~/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc}
+rm ~/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc}
 sp=$(dirname $0)
 ln -vfs $sp/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc} ~/
 
