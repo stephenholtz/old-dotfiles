a156a535b8f92d74b86d29a2d2db97f5ffaf3cda
Stephen Holtz <stephenlholtz@gmail.com>
2014-08-02 14:37:05 -0400

    updated new links

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Configuration Files
 - use linkall.sh for linking to local ~ dir
+-- sh ~/dotfiles/linkall.sh
 - iTerm2 settings saved, tinkering required!
 - matlab startup.m should be moved with shell script
--- linkall.sh
+++ linkall.sh
@@ -1,17 +1,29 @@
 # Link configuration files to home directory
+echo 'Link configuration files'
 
 HERE=$(dirname $0)
-echo 'sym-linking config files' $HERE 'to' $HOME
+echo '  sym-linking config files' $HERE 'to' $HOME
 rm $HOME/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc}
 ln -sv $HERE/{.vimrc,.zshrc,.tmux.conf,.bash_profile,.taskrc} $HOME
 
-# Link matlab startup file for correct startup config
+# links not in home directory
+
+# Elinks conf
+echo '  sym-linking elinks.conf'
+rm $HOME/.elinks/elinks.conf
+ln -sv $HERE/elinks.conf $HOME/.elinks/elinks.conf
+
+# irssi scripts and conf
+echo '  sym-linking irssi scripts'
+mkdir -p $HOME/.irssi/scripts/autorun
+ln -sv $HERE/irssi_autorun/{spell.pl,nicklist.pl} $HOME/.irssi/scripts/autorun
+
+# matlab startup file
+echo '  sym-linking matlab startup'
 rm $HOME/Documents/MATLAB/startup.m
 ln -sv $HERE/startup.m $HOME/Documents/MATLAB/startup.m
 
 # update my vim bundles
+echo 'Updating vim bundles'
 vim +BundleInstall! +BundleClean
 
-# link my irssi scripts
-mkdir -p $HOME/.irssi/scripts/autorun
-ln -sv $HERE/irssi_autorun/{spell.pl,nicklist.pl} $HOME/.irssi/scripts/autorun
