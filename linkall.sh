dd3d659be4d55a3f0f526defaaa2fe7867fa4830
Stephen <git@holtz.email>
2014-09-27 15:33:02 -0400

    final changes

--- linkall.sh
+++ linkall.sh
@@ -1,15 +1,15 @@
 # Link configuration files to home directory
 echo '>>> Linking configuration files and folders'
-echo ''
 
-HERE=${HOME}/dotfiles/
-echo '>>> sym-linking config files' $HERE 'to' $HOME
-rm -rv $HOME/{.vimrc,.tmux.conf,.bash_profile,.taskrc,.gitconfig}
-ln -sv $HERE/{.vimrc,.tmux.conf,.bash_profile,.taskrc,.gitconfig} $HOME
-rm -rv $HOME/.zshrc
-ln -sv $HERE/zsh/zshrc $HOME/.zshrc
-rm -rv $HOME/.tmuxinator
-ln -sv $HERE/zsh/tmuxinator $HOME/.tmuxinator
+DOTFILES="${HOME}/dotfiles/"
+echo '>>> sym-linking config files'
+ln -sfv $DOTFILES/gitconfig $HOME/.gitconfig
+ln -sfv $DOTFILES/tmux.conf $HOME/.tmux.conf
+ln -sfv $DOTFILES/bash_profile $HOME/.bash_profile
+ln -sfv $DOTFILES/taskrc $HOME/.taskrc
+ln -sfv $DOTFILES/vimrc $HOME/.vimrc
+ln -sfv $DOTFILES/zsh/zshrc $HOME/.zshrc
+ln -sfv $DOTFILES/zsh/tmuxinator $HOME/.tmuxinator
 
 # Link colors
 echo '>>> sym-linking shell colors'
@@ -17,7 +17,7 @@ if [ -d $HOME/.colors/ ]; then
   rm -vrf $HOME/.colors/
 fi
 mkdir -vp $HOME/.colors/
-ln -sv $HERE/colors/* $HOME/.colors/
+ln -sv $DOTFILES/colors/* $HOME/.colors/
 
 # Link vundle 
 echo '>>> sym-linking Vundle'
@@ -28,12 +28,12 @@ else
       rm -rf $HOME/.vim/bundle/Vundle.vim
     fi
 fi
-ln -sv $HERE/Vundle.vim $HOME/.vim/bundle/Vundle.vim
+ln -sv $DOTFILES/Vundle.vim $HOME/.vim/bundle/Vundle.vim
 
 # Link antigen
 echo '>>> sym-linking antigen'
 rm -rf $HOME/.antigen
-ln -sv $HERE/antigen $HOME/.antigen
+ln -sv $DOTFILES/antigen $HOME/.antigen
 
 # links not in home directory
 # Elinks conf
@@ -43,7 +43,7 @@ if [ ! -d $HOME/.elinks/ ]; then
 else
   rm -v $HOME/.elinks/elinks.conf
 fi
-ln -sv $HERE/elinks.conf $HOME/.elinks/elinks.conf
+ln -sv $DOTFILES/elinks.conf $HOME/.elinks/elinks.conf
 
 # weechat scripts and conf (ignore the log files), 
 echo '>>> sym-linking weechat folder (preserve ~/.weechat/irc.conf)'
@@ -54,8 +54,8 @@ if [ -d $HOME/.weechat/ ]; then
     rm -rv $HOME/.weechat/
 fi
 mkdir -vp $HOME/.weechat/
-ln -sv $HERE/weechat/* $HOME/.weechat/
-if [ -f $HOME/.temp.irc.conf ]; then
+ln -sv $DOTFILES/weechat/* $HOME/.weechat/
+if [ -f $DOTFILES/.temp.irc.conf ]; then
     mv $HOME/.temp.irc.conf $HOME/.weechat/irc.conf 
 fi
 
@@ -63,7 +63,7 @@ fi
 echo '>>> sym-linking bitlbee.conf '
 BITLBEE_CONF_PATH=$( brew list bitlbee | grep '/etc\/bitlbee/bitlbee.conf' )
 rm -rv $BITLBEE_CONF_PATH
-ln -sv $HERE/bitlbee.conf $BITLBEE_CONF_PATH
+ln -sv $DOTFILES/bitlbee.conf $BITLBEE_CONF_PATH
 
 # matlab startup file
 echo '>>> sym-linking matlab startup'
