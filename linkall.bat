4046fc8db5e6e9c2251b7dbee666014bbcf00156
Stephen Holtz <git@holtz.email>
2014-10-29 15:35:02 -0400

    add linking script for windows

--- linkall.bat
+++ linkall.bat
@@ -3,6 +3,6 @@ rem Should run as an administrator
 rem 
 
 % make the keyboard work better over synergy 
-mklink /D %0/win7/macbook_keyboard.ahk / %UserProfile%/Documents 
+mklink /D %0/win/macbook_keyboard.ahk / %UserProfile%/Documents 
 % give matlab a startup file
-mklink /D %0/win7/startup.m %UserProfile%/Documents
+mklink /D %0/win/startup.m %UserProfile%/Documents
